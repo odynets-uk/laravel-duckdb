03cc08f1059a636fc844f8189a52ddd44dce3798
odynets-uk <the.odynets@gmail.com>
2026-01-27 23:05:14 +0200

    Update LaravelDuckdbConnection.php

--- src/LaravelDuckdbConnection.php
+++ src/LaravelDuckdbConnection.php
@@ -183,7 +183,7 @@ class LaravelDuckdbConnection extends PostgresConnection
 
     public function getDefaultQueryGrammar()
     {
-        return $this->withTablePrefix(new QueryGrammar);
+        return $this->withTablePrefix(new QueryGrammar($this));
     }
 
     public function useDefaultPostProcessor()
@@ -217,7 +217,7 @@ class LaravelDuckdbConnection extends PostgresConnection
 
     protected function getDefaultSchemaGrammar()
     {
-        return new SchemaGrammar;
+        return new SchemaGrammar($this);
     }
 
     /**
